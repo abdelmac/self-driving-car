d2a7cefb97fbee558c9ebad6a9fbe637c880efe3
abdelmac <abdelkadermachta@gmail.com>
2024-10-13 21:22:12 +0200

    er

--- car.js
+++ car.js
@@ -1,24 +1,26 @@
-class Car {
-    constructor(x, y, width, height, controlType,maxSpeed=3) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+class Car{
+    constructor(x,y,width,height,controlType,maxSpeed=3){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
 
         this.speed=0;
         this.acceleration=0.2;
         this.maxSpeed=maxSpeed;
         this.friction=0.05;
         this.angle=0;
-
-        
-        this.controls = new Controls(controlType);
-
         this.damaged=false;
+
+        this.useBrain=controlType=="AI";
 
         if(controlType!="DUMMY"){
             this.sensor=new Sensor(this);
+            this.brain=new NeuralNetwork(
+                [this.sensor.rayCount,6,4]
+            );
         }
+        this.controls=new Controls(controlType);
     }
 
     update(roadBorders,traffic){
@@ -26,10 +28,20 @@ class Car {
             this.#move();
             this.polygon=this.#createPolygon();
             this.damaged=this.#assessDamage(roadBorders,traffic);
-            
         }
         if(this.sensor){
             this.sensor.update(roadBorders,traffic);
+            const offsets=this.sensor.readings.map(
+                s=>s==null?0:1-s.offset
+            );
+            const outputs=NeuralNetwork.feedForward(offsets,this.brain);
+
+            if(this.useBrain){
+                this.controls.forward=outputs[0];
+                this.controls.left=outputs[1];
+                this.controls.right=outputs[2];
+                this.controls.reverse=outputs[3];
+            }
         }
     }
 
@@ -77,12 +89,14 @@ class Car {
         if(this.controls.reverse){
             this.speed-=this.acceleration;
         }
+
         if(this.speed>this.maxSpeed){
             this.speed=this.maxSpeed;
         }
         if(this.speed<-this.maxSpeed/2){
             this.speed=-this.maxSpeed/2;
         }
+
         if(this.speed>0){
             this.speed-=this.friction;
         }
@@ -92,9 +106,9 @@ class Car {
         if(Math.abs(this.speed)<this.friction){
             this.speed=0;
         }
+
         if(this.speed!=0){
             const flip=this.speed>0?1:-1;
-        
             if(this.controls.left){
                 this.angle+=0.03*flip;
             }
@@ -107,13 +121,12 @@ class Car {
         this.y-=Math.cos(this.angle)*this.speed;
     }
 
-    draw(ctx,color) {
+    draw(ctx,color){
         if(this.damaged){
             ctx.fillStyle="gray";
         }else{
             ctx.fillStyle=color;
         }
-
         ctx.beginPath();
         ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
         for(let i=1;i<this.polygon.length;i++){
@@ -122,13 +135,7 @@ class Car {
         ctx.fill();
 
         if(this.sensor){
-            this.sensor.draw(ctx);  
+            this.sensor.draw(ctx);
         }
-
-        
     }
-
 }
-
-
-
