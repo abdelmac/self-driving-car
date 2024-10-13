2866156592507a461b43a9215a6ba4710fa625bf
abdelmac <abdelkadermachta@gmail.com>
2024-10-13 23:47:22 +0200

    dz

--- utils.js
+++ utils.js
@@ -44,5 +44,5 @@ function getRGBA(value) {
     const R = value < 0 ? 0 : 255;
     const G = R;
     const B = value > 0 ? 0 : 255;
-    ctx.strokeStyle = "rgba(" + R + "," + G + "," + B + "," + alpha + ")";
+    return "rgba(" + R + "," + G + "," + B + "," + alpha + ")";
 }           
